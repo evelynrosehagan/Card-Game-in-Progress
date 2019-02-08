efd617d70ce7997bdd75d8e9243739be22334dde
Evelyn Hagan <evelynrosehagan@gmail.com>
2019-02-08 13:54:06 -0500

    Commit before break

--- GenericCard/src/basegame/cardutilities/DeckSearchUtility.cs
+++ GenericCard/src/basegame/cardutilities/DeckSearchUtility.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using GenericCard.src.basegame.cardsystem;
 namespace GenericCard.src.basegame.cardutilities
 {
     class DeckSearchUtility
     {
+        public bool DeckHasCard(AbstractCard card, AbstractDeck deck)
+        {
+            AbstractCard[] CardList = deck.GetArrayOfCardsInDeck();
+            for (int i = 0; i < CardList.Length; i++)
+                if (CardList[i].GetID() == card.GetID())
+                    return true;
+            return false;
+        }
     }
 }
