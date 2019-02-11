79f588335f8d0464297a6f61c90f3f3da26310e4
Evelyn Hagan <evelynrosehagan@gmail.com>
2019-02-11 14:22:57 -0500

    Added some detail to game model

--- GenericCard/src/amvcc/GameModel.cs
+++ GenericCard/src/amvcc/GameModel.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using GenericCard.src.amvcc.model;
+using GenericCard.src.basegame.battlesystem;
+using GenericCard.src.basegame.charactersystem;
 namespace GenericCard.src.amvcc
 {
     class GameModel : GameEntity
     {
+        public AbstractPlayerCharacter player_character;
+
+        CombatModel combat_model;
+        OverworldModel overworld_model;
+
+        public enum CurrentModelState
+        {
+            Overworld,
+            Combat,
+            Shop
+        }
     }
 }
---
+++ GenericCard/src/amvcc/model/CombatModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GenericCard.src.basegame.charactersystem;
+using GenericCard.src.basegame.battlesystem;
+namespace GenericCard.src.amvcc.model
+{
+    class CombatModel : GameEntity
+    {
+        AbstractPlayerCharacter PlayerCharacter;
+        AbstractMonster MonsterCharacter;
+        AbstractEnvironment EnvironmentCharacter;
+
+        AbstractArena Arena;
+    }
+}
